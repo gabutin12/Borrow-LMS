cd000feac4bcb0aa338ecaff7c2ead52a562b671
Jundel-Malazarte <jundelmalazarte348@gmail.com>
2025-05-13 08:29:29 +0800

    Updating the library_db sql admin table

--- script/dbSQL/library_db.sql
+++ script/dbSQL/library_db.sql
@@ -43,7 +43,6 @@ ADD COLUMN `email` varchar(100) DEFAULT NULL AFTER `lastname`,
 ADD COLUMN `photo` varchar(255) DEFAULT 'images/default.jpg' AFTER `email`,
 ADD COLUMN `status` enum('Active','Inactive') DEFAULT 'Active' AFTER `photo`;
 
-
 --
 -- Dumping data for table `admin`
 --
